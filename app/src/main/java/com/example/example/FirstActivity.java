22a22cc53a647f19d51a3340100f33cd99d98839
do phong <kenzjx37@gmail.com>
2022-04-03 17:37:22 +0700

    vongdoi

--- app/src/main/java/com/example/example/FirstActivity.java
+++ app/src/main/java/com/example/example/FirstActivity.java
@@ -1,4 +1,70 @@
 package com.example.example;
 
-public class FirstActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FirstActivity extends AppCompatActivity {
+
+    private String Tag ="First ActivityLog";
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(Tag, "OnCreate");
+        setContentView(R.layout.layout_firstactivity);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Toast.makeText( FirstActivity.this,
+//                 "ban bam vao nut bam",
+//                Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent();
+                intent.setClass(FirstActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(Tag, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(Tag, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(Tag, "onPause");
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(Tag, "onRestart");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(Tag, "onDestroy");
+
+    }
 }
